8fd6c11940d939f9914c708a9128eee4afd89605
ANASTASIIA ANDRUSHKEVYCH <anastasiia.andrushkevych@microsoft.wsei.edu.pl>
2022-03-23 19:57:24 +0100

    edit

--- projekt 2 - notekeep/src/index.ts
+++ projekt 2 - notekeep/src/index.ts
@@ -3,14 +3,43 @@ import {Request, Response} from 'express'
 
 const app = express()
 
+const notes: Note[] = []
+
+class Note {
+  title: string;
+  content:string;
+  //createDate?: string
+  tags?: string[];
+  id?: number;
+}
+
+class Tag{
+  id?: number;
+  name: string;
+}
+
 app.use(express.json())
 
-app.get('/', function (req: Request, res: Response) {
+app.get('/', function (req, res) {
+  const note = { title: 'book'}
+  const jsonNote = JSON.stringify(note)
+  res.send(note)
+})
+
+app.post('/', function (req: Request, res:Response) {
+  const title = req.body.title 
+  res.status(200).send('note title: ${title}')
+})
+
+app.put('/', function (req: Request, res:Response) {
+
+  //const jsonNote = JSON.stringify(note)
+  //res.send(note)
+})
+
+app.delete('/', function (req, res) {
   res.send('GET Hello World')
 })
-app.post('/', function (req: Request, res: Response) {
-  console.log(req.body) // e.x. req.body.title 
-  res.status(200).send('POST Hello World')
-})
+
 
 app.listen(3000)
