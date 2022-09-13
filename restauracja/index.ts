05475c50af178e64f87428bcdecc372895ca852d
nastiandru <nastia.andr070703gmail.com>
2022-09-14 01:48:40 +0300

    Create index.ts

---
+++ restauracja/index.ts
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+
+import { RestaurantRepository } from "./RestauracjaCRUD";
+
+
+const restaurantRepository = new RestaurantRepository();
+
+// if restaurant collection is empty, populate it with some data
+population();
+
+async function population() 
+{
+    // check if restaurant collection exists
+    await mongoose.connection.db.listCollections({ name: 'Restaurant' })
+        .next(function (err: any, collinfo: any) 
+        {
+            if (collinfo) 
+            {
+                console.log("Restaurant collection exists");
+            } 
+            else 
+            {
+                restaurantRepository.populateRestaurants();
+            }
+        });
+}
