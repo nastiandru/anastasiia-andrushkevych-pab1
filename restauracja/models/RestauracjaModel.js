d8b966983778a6d41cd28de706e9c9f36d130f9e
nastiandru <nastia.andr070703gmail.com>
2022-09-14 12:44:35 +0300

    Create RestauracjaModel.js

---
+++ restauracja/models/RestauracjaModel.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var Restauraunt = /** @class */ (function () {
+    function Restauraunt(nazwa, adres, telefon, nip, email, www) {
+        this.nazwa = nazwa;
+        this.adres = adres;
+        this.telefon = telefon;
+        this.nip = nip;
+        this.email = email;
+        this.www = www;
+    }
+    return Restauraunt;
+}());
+exports["default"] = Restauraunt;
