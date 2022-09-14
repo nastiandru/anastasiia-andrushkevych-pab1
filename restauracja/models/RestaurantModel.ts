8a7bd94326d1dab26ddc6a88ee96e1b4e6cec41d
nastiandru <nastia.andr070703gmail.com>
2022-09-14 14:40:22 +0300

    Create RestaurantModel.ts

---
+++ restauracja/models/RestaurantModel.ts
@@ -0,0 +1,21 @@
+ class Restaurant{
+
+     name: string;
+     address: string;
+     phone: string;
+     nip: string;
+     email: string;
+     www: string;
+
+     constructor(name: string, address: string, phone: string, nip: string, email: string, www: string)
+     {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.nip = nip;
+        this.email = email;
+        this.www = www;
+     }
+ }
+
+ export default Restaurant;
