e5ab57533ddce7509177e2f16d988ce28ae5beef
nastiandru <nastia.andr070703gmail.com>
2022-09-14 13:05:22 +0300

    Update PracownikModel.ts

--- restauracja/models/PracownikModel.ts
+++ restauracja/models/PracownikModel.ts
@@ -1,14 +1,19 @@
-class PracownikModel
+import Restaurant from "./RestauracjaModel"
+
+class Pracownik
 {
     imie: string;
     nazwisko: string;
-    stanowisko: string
+    stanowisko: string;
+    restauracja: Restaurant;
+    
 
-    constructor(name: string, surname: string, stanowisko: string)
+    constructor(imie: string, nazwisko: string, stanowisko: string, restauracja: Restaurant)
     {
-        this.imie = name;
-        this.nazwisko = surname;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
         this.stanowisko = stanowisko;
+        this.restauracja = restauracja;
     }
 }
-export default PracownikModel;
+export default Pracownik;
