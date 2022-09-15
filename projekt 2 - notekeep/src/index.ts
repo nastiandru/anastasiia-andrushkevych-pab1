24462e1038db83d3882cc4f3da2ffcc5da7e2f8b
nastiandru <nastia.andr070703gmail.com>
2022-09-15 11:39:48 +0300

    Update index.ts

--- projekt 2 - notekeep/src/index.ts
+++ projekt 2 - notekeep/src/index.ts
@@ -1,46 +1,85 @@
-import express from 'express'
-import {Request, Response} from 'express'
+import express = require ('express');
+import {Request, Response} from 'express';
+import Note from './models/note';
 
-const app = express()
-var router = express.Router()
-const notes: Note[] = []
+const app = express();
 
-class Note {
-  title: string;
-  content:string;
-  //createDate?: string
-  tags?: string[];
-  id?: number;
+app.use(express.json());
+
+const notes : Note[] = [];
+
+// Dodanie nowej notatki
+app.post('/note', function(req : Request, res : Response)
+{
+    const note : Note = req.body;
+    if(note.title === undefined || note.content === undefined)
+    {
+        res.status(400).send('Missing note title/content')
+    }
+    else
+    {
+        note.id=Date.now();
+        notes.push(note);
+        res.status(201).send(note.id);
+    }
+})
+
+// Odczytanie notatki o danym id
+app.get('/note/:id', function(req : Request,res : Response)
+{
+  const note = notes.find(i=>i.id === req.body.id);
+  if(note===undefined)
+  {
+    res.status(404).send('Note of this id does not exist');
+  }
+  else
+  {
+    res.status(200).send(note)
+  }
+})
+
+// Edycja notatki o danym id
+app.put('/note/:id', function(req: Request, res: Response)
+{
+  const newNote: Note = req.body;
+  if(newNote.title === undefined || newNote.content === undefined || newNote.id === undefined)
+  {
+    res.status(400).send('Missing note title/content/id');
+  } 
+  else
+  {
+    let currentNote: Note = notes.find(i=>i.id === newNote.id)
+    if(currentNote === undefined)
+    {
+      res.status(404).send('Note of this id does not exist');
+    }
+    else
+    {
+      currentNote = newNote;
+      res.status(200).send(currentNote);
+    }
+  }
+})
+
+//TODO: getting id from url
+// Usunięcie notatki o danym id
+app.delete('/note/:id', function(req : Request, res : Response)
+{
+  const note: Note = notes.find(i=>i.id == req.body.id);
+  if(note === undefined)
+  {
+    res.status(400).send('Note of this id does not exist');
+  }
+  else
+  {
+    notes.splice(req.body.id, 1);
+    res.status(204).send('The requested note has been deleted')
+  }
+})
+
+function noteMissing(res : Response)
+{
+  res.status(404).send('Note of this id does not exist');
 }
 
-interface DataStorage {}
-class Tag{
-  id?: number;
-  name: string;
-}
-
-app.use(express.json())
-
-app.get('/', function (req, res) {
-  const note = { title: 'book'}
-  const jsonNote = JSON.stringify(note)
-  res.send(note)
-})
-
-app.post('/', function (req: Request, res:Response) {
-  const title = req.body.title 
-  res.status(200).send('note title: ${title}')
-})
-
-app.put('/', function (req: Request, res:Response) {
-
-  //const jsonNote = JSON.stringify(note)
-  //res.send(note)
-})
-
-app.delete('/', function (req, res) {
-  res.send('GET Hello World')
-})
-
-
-app.listen(3000)
+app.listen(3002)
